cc20506ea5341d21bf66efbb900526ed5cf02f8e
Doncho Minkov <DonchoMinkov@gmail.com>
2019-04-08 22:47:51 +0300

    Added db

---
+++ src/main/java/org/softuni/deploymentdemo/Product.java
@@ -0,0 +1,21 @@
+package org.softuni.deploymentdemo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+    private long id;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
